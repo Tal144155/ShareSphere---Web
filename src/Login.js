f06fbb00f18a9f9edde537567beda75d346601f0
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-01 11:09:40 +0200

    editing login page

--- src/Login.js
+++ src/Login.js
@@ -2,34 +2,45 @@ import "./Login.css";
 import { Link } from "react-router-dom";
 function Login() {
   return (
-    <div className="col">
-      <div class="card text-center mb-3" id="card-style">
-        <div class="card-body">
-          <div class="mb-3">
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="Email or phone number"
-            ></input>
-            <input
-              type="password"
-              id="password"
-              placeholder="Password"
-              class="form-control"
-              aria-describedby="passwordHelpBlock"
-            ></input>
+    <div id="login-grid" className="container text-center">
+      <div className="row">
+        <div className="col" id="name-slogen">
+          <div id="app-name">ShareSphere</div>
+          <div id="slogen">
+            Connect with friends and the world
+            <br /> around you on ShareSphere
           </div>
-          <div class="d-grid gap-2 col-12 mx-auto">
-            <a href="/" class="btn btn-primary" type="button">
-              Login
-            </a>
+        </div>
+        <div className="col">
+          <div class="card text-center mb-3" id="card-style">
+            <div class="card-body">
+              <div class="mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  placeholder="Email or phone number"
+                ></input>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  class="form-control"
+                  aria-describedby="passwordHelpBlock"
+                ></input>
+              </div>
+              <div class="d-grid gap-2 col-12 mx-auto">
+                <a href="/" class="btn btn-primary" type="button">
+                  Login
+                </a>
+              </div>
+              <div id="forgot">
+                <text>Forgot password?</text>
+              </div>
+              <hr id="border-line"></hr>
+              <Link to="/register" id="new-account" class="btn btn-primary" type="submit">Create new account</Link>
+            </div>
           </div>
-          <div id="forgot">
-            <text>Forgot password?</text>
-          </div>
-          <hr id="border-line"></hr>
-          <Link to="/App" id="new-account" class="btn btn-primary" type="submit">Create new account</Link>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import EnterPage from './EnterPage';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <EnterPage />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<App/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
