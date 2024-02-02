9f79fdd8978edb65d6f3d4dadc83b44c84d29302
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-02 14:18:15 +0200

    deleting userchecks

--- src/UserCheck.js
+++
@@ -1,8 +0,0 @@
-
-
-function UserCheck(props) {
-    const list = props.usersList.filter((user) => user.user_name===props.user_name && user.password===props.password);
-  console.log("----------");
-}
-
-export default UserCheck;
