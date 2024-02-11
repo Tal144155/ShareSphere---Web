d4e06f714ac430d44ab3650c342b7dbee51f53dd
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-11 21:56:07 +0200

    first test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders login page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Connect with friends and the world around you on ShareSphere/i);
+  const linkElement = screen.getByText(
+    /Connect with friends and the world around you on ShareSphere/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
