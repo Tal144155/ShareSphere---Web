8ffc3a1150e0b38be8fe907835756532b43addab
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-15 01:04:57 +0200

    add+show comments comments

--- src/FeedPage/Comments/CommentText.js
+++ src/FeedPage/Comments/CommentText.js
@@ -1,19 +1,25 @@
-import './AddComment.css'
+import "./AddComment.css";
 
 const CommentText = (props) => {
-    const handleChange = (e) => {
-        props.setInputFields({ ...props.inputFields, [e.target.name]: e.target.value });
-      };
-    return(
-    <textarea id="comment-text-add"
-                    type="text"
-                    name="comment"
-                    className="form-control"
-                    placeholder="What do you think about the post?"
-                    onChange={handleChange}
-                    value={props.inputFields.comment}
-                  ></textarea >
-    );
-}
+  //setting the input list on every change
+
+  const handleChange = (e) => {
+    props.setInputFields({
+      ...props.inputFields,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <textarea
+      id="comment-text-add"
+      type="text"
+      name="comment"
+      className="form-control"
+      placeholder="What do you think about the post?"
+      onChange={handleChange}
+      value={props.inputFields.comment}
+    ></textarea>
+  );
+};
 
 export default CommentText;
