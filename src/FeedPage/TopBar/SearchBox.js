d44b26c1a55c3c054b39ef5fab114a270b014fb6
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-15 01:29:45 +0200

    search boc comments

--- src/FeedPage/TopBar/SearchBox.js
+++ src/FeedPage/TopBar/SearchBox.js
@@ -1,6 +1,7 @@
 import './SearchBox.css'
 
 const SearchBox = () => {
+  //shwing the search box
   return (
     <form className="d-flex" role="search" id="search">
     <input id="search-box" className="form-control me-2" type="search" placeholder="Search..." aria-label="Search"></input>
