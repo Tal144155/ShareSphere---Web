3bfc42a07b326ef23a2147ed5911ceadee162118
Tal144155 <tal.ariel.ziv@gmail.com>
2024-02-15 01:17:37 +0200

    logout button comments

--- src/FeedPage/LogOut/logoutbutton.js
+++ src/FeedPage/LogOut/logoutbutton.js
@@ -4,14 +4,17 @@ import { useNavigate } from "react-router-dom";
 const LogOutButton = (props) => {
   const navigate = useNavigate();
 
+  //when the button is pressed, log out
   const handlelogout = (event) => {
     event.preventDefault();
+    //set the loged in user to nothing
     props.setlogedinuser({
       username: "",
       first_name: "",
       last_name: "",
       user_pic: "",
     });
+    //navigate to the login
     const newRoute = "/";
     navigate(newRoute);
   };
